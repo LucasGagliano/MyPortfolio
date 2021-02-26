9c86ad7f271d69a06685ca9c77f49ef3e5bfedf4
Lucas Martin <31822176+LucasGagliano@users.noreply.github.com>
2021-02-25 22:37:28 -0300

    Correção de bugs e Jogos.html / 25-02-2021
    Upload de correções de bugs e upload do projeto do pong na página de jogos, além da remoção da página de fotos

--- Codigos/JS/script.js
+++ Codigos/JS/script.js
@@ -1,10 +1,3 @@
-console.log("Hello, World!");
-
-function AtivarSobreposicao()
-{
-    document.getElementById("Rosquinha").style.display = "block";
-}
-function DesativarSobreposicao()
-{
-    document.getElementById("Rosquinha").style.display = "none";
-}
+console.log("Se você está aqui, sinta-se a vontade parar estudar o código!");
+console.log("Eu o escrevi em uma linguagem chamada 'HAML', que em pouco muda de 'HTML'");
+console.log("Dica: eu modifiquei o bootstrap que usei no site, adicionando algumas classes a mais que podem ser úteis. Se você desejar, sinta-se livre para utilizá-lo, seguindo, claro, todas as normas inicialmente impostas pela W3C");
